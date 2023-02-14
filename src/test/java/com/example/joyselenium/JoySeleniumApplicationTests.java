de3ab1a18b17f4870ceb51a03f81308ea3bb9863
cristian <marcial2005@gmail.com>
2023-02-14 10:16:44 -0600

    Revert "Revert "First commmit""
    This reverts commit c00cf25faa92b47828f34e0c47d3723da5f23dc6.

---
+++ src/test/java/com/example/joyselenium/JoySeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joyselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoySeleniumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
